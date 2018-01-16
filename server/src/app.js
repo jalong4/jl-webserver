21a9bd6a598620768443ebee311b6d87faf716b6
Jim Long <jalong4@hotmail.com>
2018-01-15 19:14:30 -0800

    add gitignore file to exclude node_modules

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,30 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+// const path = require('path')
 
-var app = express()
-// Body Parser Middleware
+const app = express()
 
+// Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('combined'))
+app.use(cors())
 
-// Set Static Path
-app.use(express.static(path.join(__dirname, 'client')))
 // Routes
 app.get('/', function (req, res) {
 
 })
 
-const port = 3000
+app.get('/status', function (req, res) {
+  res.send({
+    message: 'Server is available'
+  })
+})
+
+const port = process.env.PORT || 3000
 
 app.listen(port, function () {
-  console.log('Server started on port', port)
+  console.log('Server started on port ', port)
 })
