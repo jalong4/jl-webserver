7a3a54d1a2a5d808565727afa569f89fe294ad7f
Jim Long <jalong4@hotmail.com>
2018-01-22 10:13:22 -0800

    Refactor Home view

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,71 +2,31 @@
   <v-container fluid grid-list-md>
     <v-layout>
       <v-flex xs8 class="mr-2 mb-2">
-        <v-card>
-          <v-toolbar color="cyan" dark>
-            <v-toolbar-title>Media</v-toolbar-title>
-             <v-spacer></v-spacer>
-          <v-btn icon dark>
-            <v-icon>search</v-icon>
-          </v-btn>
-          </v-toolbar>
-          <v-layout row wrap>
-            <v-flex xs3 v-for="mediaItem in media" :key="mediaItem._id">
-              <v-card>
-                <v-card-media :src="mediaItem.url" height="200px">
-                </v-card-media>
-                <v-card-actions class="white">
-                  <div v-text="mediaItem.filename"></div>
-                  <v-spacer></v-spacer>
-                  <v-btn icon>
-                    <v-icon>favorite</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>bookmark</v-icon>
-                  </v-btn>
-                  <v-btn icon>
-                    <v-icon>share</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-card>
+          <media :media="media"/>
       </v-flex>
 
       <v-flex xs4>
         <v-date-picker color="cyan" landscape></v-date-picker>
       </v-flex>
-
     </v-layout>
 
     <v-layout>
       <v-flex xs8  class="mr-2">
-        <v-card>
-          <v-toolbar color="cyan" dark>
-            <v-toolbar-title>Projects</v-toolbar-title>
-          </v-toolbar>
-          <v-list two-line subheader>
-            <v-list-tile v-for="project in projects" :key="project._id">
-              <v-list-tile-content>
-                <v-list-tile-title>{{project.name}}</v-list-tile-title>
-                <v-list-tile-sub-title>{{project.description}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
+        <projects :projects="projects"/>
       </v-flex>
       <v-flex xs4>
         <v-time-picker color="cyan" landscape></v-time-picker>
       </v-flex>
     </v-layout>
-  </v-container>
 
+  </v-container>
 </template>
 <script>
 
-import Media from '@/services/MediaServices'
-import Projects from '@/services/ProjectsServices'
+import Media from '@/components/Media'
+import Projects from '@/components/Projects'
+import MediaServices from '@/services/MediaServices'
+import ProjectsServices from '@/services/ProjectsServices'
 
 export default {
   data () {
@@ -77,16 +37,13 @@ export default {
   },
   methods: {
   },
+  components: {
+    Media,
+    Projects
+  },
   async mounted () {
-    this.media = (await Media.index()).data
-    this.projects = (await Projects.index()).data
+    this.media = (await MediaServices.index()).data
+    this.projects = (await ProjectsServices.index()).data
   }
 }
 </script>
-
-<style scoped>
-
-.list__tile__sub-title {
-  white-space: pre-line;
-}
-</style>
