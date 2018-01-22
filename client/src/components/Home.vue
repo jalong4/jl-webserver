a28954a1c0115cee80bcbb26074a8ce6f391ff4c
Jim Long <jalong4@hotmail.com>
2018-01-21 20:42:59 -0800

    Update layout of home view

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,32 +1,13 @@
 <template>
   <v-container fluid grid-list-md>
-  <v-layout>
-    <v-flex xs6>
-      <v-date-picker color="cyan" landscape></v-date-picker>
-      <br><br>
-      <v-card>
-        <v-toolbar color="cyan" dark>
-          <v-toolbar-title>Projects</v-toolbar-title>
-        </v-toolbar>
-        <v-list two-line subheader>
-          <v-list-tile v-for="project in projects" :key="project._id">
-            <v-list-tile-content>
-              <v-list-tile-title>{{project.name}}</v-list-tile-title>
-              <v-list-tile-sub-title>{{project.description}}</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-card>
-    </v-flex>
-    <v-flex>
-    </v-flex>
-    <v-flex xs6>
-      <v-card>
-        <v-toolbar color="cyan" dark>
-          <v-toolbar-title>Media</v-toolbar-title>
-        </v-toolbar>
+    <v-layout>
+      <v-flex xs8 class="mr-2 mb-2">
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Media</v-toolbar-title>
+          </v-toolbar>
           <v-layout row wrap>
-            <v-flex xs6 v-for="mediaItem in media" :key="mediaItem._id">
+            <v-flex xs3 v-for="mediaItem in media" :key="mediaItem._id">
               <v-card>
                 <v-card-media :src="mediaItem.url" height="200px">
                   <v-container fill-height fluid>
@@ -37,7 +18,7 @@
                     </v-layout>
                   </v-container>
                 </v-card-media>
-                <!-- <v-card-actions class="white">
+                <v-card-actions class="white">
                   <v-spacer></v-spacer>
                   <v-btn icon>
                     <v-icon>favorite</v-icon>
@@ -48,14 +29,41 @@
                   <v-btn icon>
                     <v-icon>share</v-icon>
                   </v-btn>
-                </v-card-actions> -->
+                </v-card-actions>
               </v-card>
             </v-flex>
           </v-layout>
         </v-card>
       </v-flex>
+
+      <v-flex xs4>
+        <v-date-picker color="cyan" landscape></v-date-picker>
+      </v-flex>
+
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs8  class="mr-2">
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Projects</v-toolbar-title>
+          </v-toolbar>
+          <v-list two-line subheader>
+            <v-list-tile v-for="project in projects" :key="project._id">
+              <v-list-tile-content>
+                <v-list-tile-title>{{project.name}}</v-list-tile-title>
+                <v-list-tile-sub-title>{{project.description}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+      <v-flex xs4>
+        <v-time-picker color="cyan" landscape></v-time-picker>
+      </v-flex>
     </v-layout>
   </v-container>
+
 </template>
 <script>
 
@@ -77,3 +85,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.list__tile__sub-title {
+  white-space: pre-line;
+}
+</style>
+
